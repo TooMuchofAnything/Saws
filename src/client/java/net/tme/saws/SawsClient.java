fe93958753a0240ea0e29b3101fe159dba408501
TooMuchofEverything <toomuchofeverything@protonmail.com>
2024-01-27 16:33:26 -0500

    Initial commit

---
+++ src/client/java/net/tme/saws/SawsClient.java
@@ -0,0 +1,10 @@
+package net.tme.saws;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SawsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/tme/saws/SawsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tme.saws;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SawsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
