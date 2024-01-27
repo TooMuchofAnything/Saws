f4bfe09a24f8630a591f28e99d36496428f02f70
TooMuchofEverything <toomuchofeverything@protonmail.com>
2024-01-27 16:35:56 -0500

    Added recipe for crafting tables
    1 log -> 1 crafting table

--- src/main/java/net/tme/saws/SawsDataGenerator.java
+++ src/main/java/net/tme/saws/SawsDataGenerator.java
@@ -2,10 +2,39 @@ package net.tme.saws;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.advancement.criterion.TickCriterion;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.util.Identifier;
 
 public class SawsDataGenerator implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator generator) {
+		FabricDataGenerator.Pack pack = generator.createPack();
+
+		pack.addProvider(SawsRecipeGenerator::new);
+	}
+
+	private static class SawsRecipeGenerator extends FabricRecipeProvider {
+		private SawsRecipeGenerator(FabricDataOutput generator) {
+			super(generator);
+		}
+
+		@Override
+		public void generate(RecipeExporter exporter) {
+			// saws:crafting_table
+			ShapedRecipeJsonBuilder.create(RecipeCategory.DECORATIONS, Blocks.CRAFTING_TABLE)
+					.input('#', ItemTags.LOGS)
+					.pattern("#")
+					.criterion("unlock_right_away", TickCriterion.Conditions.createTick())
+					.showNotification(false) // Thus, unfortunately, only crafting_shaped can be used.
+					.offerTo(exporter, new Identifier("saws:crafting_table"));
+		}
 	}
 }
