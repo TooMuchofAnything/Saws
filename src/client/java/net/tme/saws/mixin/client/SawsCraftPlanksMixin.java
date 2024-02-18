e66373f3fd9e235c03c2b0d83dd0a5a15dba7b0e
TooMuchofEverything <toomuchofeverything@protonmail.com>
2024-02-18 08:40:47 -0500

    Added a mixin to edit craft_planks toast.
    Needs work tho.

---
+++ src/client/java/net/tme/saws/mixin/client/SawsCraftPlanksMixin.java
@@ -0,0 +1,12 @@
+package net.tme.saws.mixin.client;
+
+import net.minecraft.client.tutorial.CraftPlanksTutorialStepHandler;
+import net.minecraft.client.tutorial.TutorialManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(CraftPlanksTutorialStepHandler.class)
+public class SawsCraftPlanksMixin {
+    //Waht to do????
+}
