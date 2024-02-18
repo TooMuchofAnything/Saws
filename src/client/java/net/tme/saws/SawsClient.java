ea3150897b703d9362f4cfc65816c591d27c2acb
TooMuchofEverything <toomuchofeverything@protonmail.com>
2024-02-18 08:39:31 -0500

    Removed SawsEntityModelLayers.
    Part of larger change to remove sleeping bags.

--- src/client/java/net/tme/saws/SawsClient.java
+++ src/client/java/net/tme/saws/SawsClient.java
@@ -1,16 +1,10 @@
 package net.tme.saws;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
-import net.tme.saws.render.SawsEntityModelLayers;
-import net.tme.saws.render.block.entity.SleepingBagBlockEntityRenderer;
 
 public class SawsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-		EntityModelLayerRegistry.registerModelLayer(SawsEntityModelLayers.SLEEPING_BAG_HEAD, SleepingBagBlockEntityRenderer::createHeadLayer);
-		EntityModelLayerRegistry.registerModelLayer(SawsEntityModelLayers.SLEEPING_BAG_FOOT, SleepingBagBlockEntityRenderer::createFootLayer);
 	}
 }
