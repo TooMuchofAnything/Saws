fa6c81f19510286473177e2d0fa63201a9f77cb3
TooMuchofEverything <toomuchofeverything@protonmail.com>
2024-02-18 10:31:24 -0500

    Disabled the craft_planks tutorial toast.

--- src/client/java/net/tme/saws/mixin/client/SawsCraftPlanksMixin.java
+++ src/client/java/net/tme/saws/mixin/client/SawsCraftPlanksMixin.java
@@ -1,12 +1,16 @@
 package net.tme.saws.mixin.client;
 
 import net.minecraft.client.tutorial.CraftPlanksTutorialStepHandler;
-import net.minecraft.client.tutorial.TutorialManager;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CraftPlanksTutorialStepHandler.class)
 public class SawsCraftPlanksMixin {
-    //Waht to do????
+    @Redirect(method = "tick", at = @At(value = "FIELD", target =
+            "Lnet/minecraft/client/tutorial/CraftPlanksTutorialStepHandler;ticks:I", opcode = Opcodes.GETFIELD))
+    private int injected(CraftPlanksTutorialStepHandler instance) {
+        return 0;
+    } // very duct-tape fix!
 }
